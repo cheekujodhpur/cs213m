875c2c678eb4fbf2230f89d5ac8191a04733b692
Kumar Ayush <cheekujodhpur@gmail.com>
2016-01-15 10:12:26 +0530

    just this

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
 int main()
 {
-    string s = "Sandesh Kalantre has a ten inches long dick";
-    cout << sizeof(s) << endl;
     return 0;
 }
