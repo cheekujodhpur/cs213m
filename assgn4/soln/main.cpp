43618198fd6ee45a19cd005263097c3ed116daa2
Kumar Ayush <cheekujodhpur@gmail.com>
2016-03-16 18:26:41 +0530

    problem 1 solved

--- assgn4/soln/main.cpp
+++ assgn4/soln/main.cpp
@@ -7,12 +7,23 @@ int main()
     int n;
     cin >> n;
     MinMaxHeap<int> foo;
+    pred predObj;
     int temp;
     for(int i = 0;i<n;i++)
     {
         cin >> temp;
         foo.insert(temp);
     }
+    foo.print();
+    foo.deleteMin();
+    foo.print();
+    foo.deleteMax();
+    foo.print();
+    foo.deleteMax();
+    foo.print();
+    foo.deleteMin();
+    foo.print();
+    foo.deleteElems(predObj);
     foo.print();
     return 0;
 }
--- assgn4/soln/minMaxHeap.hpp
+++ assgn4/soln/minMaxHeap.hpp
@@ -11,6 +11,20 @@ void swap(T &a,T &b)
     b = temp;
 }
 
+
+class pred
+{
+    public:
+    bool toDelete(int elem);
+};
+
+bool pred::toDelete(int elem)
+{
+    if(elem%5==0)
+        return false;
+    else return true;
+}
+
 template<class T>
 class MinMaxHeap
 {
@@ -20,6 +34,38 @@ class MinMaxHeap
 
     void BubbleUpMin(int i);
     void BubbleUpMax(int i);
+    int min_child_grandchild(int i)
+    {
+        int result = i*2;
+        if(i*2+1<=size)
+        {
+            if(heap[i*2+1]<heap[result])result=i*2+1;
+        }
+        if(i*4+3<=size)
+        {
+            if(heap[i*4+3]<heap[result])result=i*4+3;
+            if(i*4+2<=size)if(heap[i*4+2]<heap[result])result=i*4+2;
+            if(i*4+1<=size)if(heap[i*4+1]<heap[result])result=i*4+1;
+            if(i*4+0<=size)if(heap[i*4+0]<heap[result])result=i*4+0;
+        }
+        return result;
+    }
+    int max_child_grandchild(int i)
+    {
+        int result = i*2;
+        if(i*2+1<=size)
+        {
+            if(heap[i*2+1]>heap[result])result=i*2+1;
+        }
+        if(i*4+3<=size)
+        {
+            if(heap[i*4+3]>heap[result])result=i*4+3;
+            if(i*4+2<=size)if(heap[i*4+2]>heap[result])result=i*4+2;
+            if(i*4+1<=size)if(heap[i*4+1]>heap[result])result=i*4+1;
+            if(i*4+0<=size)if(heap[i*4+0]>heap[result])result=i*4+0;
+        }
+        return result;
+    }
     public:
         /* This is a constructor */
         MinMaxHeap();
@@ -34,9 +80,21 @@ class MinMaxHeap
         /*Return the maximum of the objects */
         T getMax();
         /*The pred class defines a function toDelete with the signature bool toDelete(T). The function deleteElems then will delete from the min-max heap, all the elements such that toDelete returns true when called on them*/
-        //void deleteElems(pred predObject);
+        void deleteElems(pred predObject);
         void print();
 };
+
+template<class T>
+void MinMaxHeap<T>::deleteElems(pred predObject)
+{
+    MinMaxHeap<int> *second = new MinMaxHeap<int>;
+    for(int i = 1;i<=size;i++)
+    {
+        if(!predObject.toDelete(heap[i]))
+            second->insert(heap[i]);
+    }
+    *this = *second;
+}
 
 template<class T>
 MinMaxHeap<T>::MinMaxHeap()
@@ -118,13 +176,80 @@ void MinMaxHeap<T>::BubbleUpMax(int i)
 template<class T>
 void MinMaxHeap<T>::deleteMin()
 {
-    return;
+    int m,i,last;
+    T x;
+    if(size==0)
+        return;
+    heap[0] = heap[1];
+    x = heap[size];
+
+    for(i = 1,last = size/2; i<=last;)
+    {
+        m = min_child_grandchild(i);
+        if(x <= heap[m])        
+            break;
+        
+        heap[i] = heap[m];
+
+        if(m <= 2*i + 1)
+        {
+            i = m;
+            break;
+        }
+
+        if(x > heap[m/2])
+            swap(heap[m/2],x);
+
+        i = m;
+    }
+    heap[i] = x;
+    size--;
 }
 
 template<class T>
 void MinMaxHeap<T>::deleteMax()
 {
-    return;
+    int m,i,last;
+    T x;
+    if(size==0)
+        return;
+    else if(size==1)
+    {
+        heap[0]=heap[1];
+        size--;
+        return;
+    }
+    else if(size==2)
+    {
+        heap[0]=heap[2];
+        size--;
+        return;
+    }
+    heap[0] = (heap[2]>heap[3]?heap[2]:heap[3]);
+    int start = (heap[2]>heap[3]?2:3);
+    x = heap[size];
+
+    for(i = start,last = size/2; i<=last;)
+    {
+        m = max_child_grandchild(i);
+        if(x >= heap[m])        
+            break;
+        
+        heap[i] = heap[m];
+
+        if(m <= 2*i + 1)
+        {
+            i = m;
+            break;
+        }
+
+        if(x < heap[m/2])
+            swap(heap[m/2],x);
+
+        i = m;
+    }
+    heap[i] = x;
+    size--;
 }
 
 template<class T>
